b1b85f8882fc0c08e81d6c8b10338bcd2872a257
Ding Yu Lee <dingyu@logflows.com>
2021-12-03 10:52:51 +0800

    UPDATE:ios setting

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,11 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    //firebase setting
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+    }
+    //firebase setting end
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/services/local_notification_service.dart
+++ lib/services/local_notification_service.dart
@@ -3,17 +3,83 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class LocalNotificationService {
+  // ///Singleton pattern
+  // ///目的：保證一個類別只會產生一個物件，而且要提供存取該物件的統一方法
+  // static final LocalNotificationService _notificationService =
+  //     LocalNotificationService._internal();
+  // factory LocalNotificationService() {
+  //   return _notificationService;
+  // }
+  // LocalNotificationService._internal();
+
+  static void requestIOSPermissions() {
+    notificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+  }
+
+  // static void requestIOSPermissions(
+  //     FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) {
+  //   flutterLocalNotificationsPlugin
+  //       .resolvePlatformSpecificImplementation<
+  //           IOSFlutterLocalNotificationsPlugin>()
+  //       ?.requestPermissions(
+  //         alert: true,
+  //         badge: true,
+  //         sound: true,
+  //       );
+  // }
+
   static final FlutterLocalNotificationsPlugin notificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
   static void initialize(BuildContext context) {
+    final InitializationSettings initializationSettings = InitializationSettings(
+
+        ///android default icon
+        ///route should be: {project}\android\app\src\main\res\drawable\YOUR_APP_ICON.png
+        android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+
+        ///ios default setting
+        iOS: IOSInitializationSettings(
+          requestSoundPermission: false,
+          requestBadgePermission: false,
+          requestAlertPermission: false,
+        ));
+
+    notificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: (String route) async {
+      if (route != null) {
+        Navigator.of(context).pushNamed(route);
+      }
+    });
+  }
+
+  Future<void> init(BuildContext context) async {
+    //Initialization Settings for Android
+    final AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+
+    //Initialization Settings for iOS
+    final IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+    );
+
+    //InitializationSettings for initializing settings for both platforms (Android & iOS)
     final InitializationSettings initializationSettings =
         InitializationSettings(
-      android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+            android: initializationSettingsAndroid,
+            iOS: initializationSettingsIOS);
 
-      ///android default icon
-    );
-    notificationsPlugin.initialize(initializationSettings,
+    await notificationsPlugin.initialize(initializationSettings,
         onSelectNotification: (String route) async {
       if (route != null) {
         Navigator.of(context).pushNamed(route);
@@ -26,7 +92,7 @@ class LocalNotificationService {
       ///確保每次都是獨立的id，用datetime
       final id = DateTime.now().millisecondsSinceEpoch ~/ 1000;
 
-      ///
+      ///notification 的一些設置，如channel、優先級等
       final notificationDetails = NotificationDetails(
         android: AndroidNotificationDetails(
           "easyapproach",
@@ -36,11 +102,35 @@ class LocalNotificationService {
           priority: Priority.high,
         ),
       );
+      AndroidNotificationDetails _androidNotificationDetails =
+          AndroidNotificationDetails(
+        'channel ID',
+        'channel name',
+        'channel description',
+        playSound: true,
+        priority: Priority.high,
+        importance: Importance.high,
+      );
+
+      IOSNotificationDetails _iosNotificationDetails = IOSNotificationDetails(
+//         presentAlert: bool?,  // Present an alert when the notification is displayed and the application is in the foreground (only from iOS 10 onwards)
+//         presentBadge: bool?,  // Present the badge number when the notification is displayed and the application is in the foreground (only from iOS 10 onwards)
+//         presentSound: bool?,  // Play a sound when the notification is displayed and the application is in the foreground (only from iOS 10 onwards)
+//         sound: String?,  // Specifics the file path to play (only from iOS 10 onwards)
+//         badgeNumber: int?, // The application's icon badge number
+//         attachments: List<IOSNotificationAttachment>?, (only from iOS 10 onwards)
+//         subtitle: String?, //Secondary description  (only from iOS 10 onwards)
+//         threadIdentifier: String? (only from iOS 10 onwards)
+          );
+      NotificationDetails platformChannelSpecifics = NotificationDetails(
+          android: _androidNotificationDetails, iOS: _iosNotificationDetails);
+
       await notificationsPlugin.show(
         id,
         message.notification.title,
         message.notification.body,
-        notificationDetails,
+        // notificationDetails,
+        platformChannelSpecifics,
         payload: message.data["route"],
 
         ///傳route進去
